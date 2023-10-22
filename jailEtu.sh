8b8bb295d9e48e67642a390e2cff417e80aa26d2
Constantin Clerc <cclerc@florimont.ch>
2023-10-22 16:18:05 +0200

    LaunchDaemons plist

---
+++ jailEtu.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# jailEtu
+# v1.0 - Constantin CLERC
+
+# variables
+# on autorise ici des bundle ids
+# google est autorisé donc chrome pourrait être lancé, vous pouvez l'enlever si besoin
+elements=("google" "canon" "hp")
+app_files=()
+
+# func
+function check_bundle_id {
+    bundle_id=$1
+    for element in "${elements[@]}"; do
+        if [[ $bundle_id == *$element* ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+# On check si un fichier existe dans le dossier Printers et Google
+if [ "$(find /Users/etudiant/Library/Printers -mindepth 1 -maxdepth 1)" ] || [ "$(find /Users/etudiant/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Helpers -mindepth 1 -maxdepth 1)" ]; then
+    # 1. On regarde dans chaque .app et on check les bundle IDs
+    # oui mais, l'élève peut modifier le bundle ID en utilisant vscode et en modifiant la plist. C'est pour quoi en 2e étape on va verifier la signature
+    # 2. On regarde dans chaque .app et on check la signature (on utilise codesign)
+    while IFS= read -r -d '' app_path; do
+        app_files+=("$app_path")
+    done < <(find /Users/etudiant/Library/Printers /Users/etudiant/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Helpers -type d -name "*.app" -print0)
+
+    for app_path in "${app_files[@]}"; do
+        # On obtient le bundle id depuis l'Info.plist dans l'app et on definie une variable par la réponse.
+        bundle_id=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" "$app_path/Contents/Info.plist" 2>/dev/null)
+        if [[ -n $bundle_id ]] && check_bundle_id "$bundle_id"; then
+            # Vérification de la signature et récupération du Bundle ID
+            signature_output=$(codesign --display --verbose=4 "$app_path" 2>&1)
+            if echo "$signature_output" | grep -q "Identifier=$bundle_id"; then
+                echo "Signature correcte. CODESIGN BID: $bundle_id"
+            else
+                echo "Signature incorrecte, suppression de : $app_path"
+                rm -rf "$app_path"
+            fi
+        else
+            echo "Supression de $app_path car il n'as pas un Bundle ID autorisé"
+            rm -rf "$app_path"
+        fi
+    done
+fi
+
+# On a check si une app non autorisée était installée. Maintenant, il faut verifier si la binary de mas est installée.
